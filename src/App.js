766bc41dfd3fc3ccd7ea9b47a783e4b7afe40031
lakshay94132 <lakshaylimba31@gmail.com>
2025-07-27 12:31:42 +0530

    Lecture 36
    Adding a top loading bar to NewsMonkey

--- src/App.js
+++ src/App.js
@@ -9,22 +9,35 @@ import {
   Routes, 
   Route 
 } from "react-router-dom";
+import LoadingBar from "react-top-loading-bar";
+
 
 export default class App extends Component {
   pageSize = 12;
+  state = {
+    progress: 0
+  }
+  setProgress = (progress) => {
+    this.setState({progress: progress});
+  }
   render() {
     return (
       <Router>
         <Navbar />
+        <LoadingBar
+        color="#f11946"
+        progress={this.state.progress}
+      />
+
         <Routes>
-          <Route path="/" element={<News key="general" pageSize = {this.pageSize} country="us" category="General" />} />
-          <Route path="/Business" element={<News key="business" pageSize = {this.pageSize} country="us" category="Business" />} />
-          <Route path="/Entertainment" element={<News key="entertainment" pageSize = {this.pageSize} country="us" category="Entertainment" />} />
-          <Route path="/Sports" element={<News key="sports" pageSize = {this.pageSize} country="us" category="Sports" />} />
-          <Route path="/Health" element={<News key="health" pageSize = {this.pageSize} country="us" category="Health" />} />
-          <Route path="/Science" element={<News key="science" pageSize = {this.pageSize} country="us" category="Science" />} />
-          <Route path="/Technology" element={<News key="technology" pageSize = {this.pageSize} country="us" category="Technology" />} />
-          <Route path="/General" element={<News key="general" pageSize = {this.pageSize} country="us" category="General" />} />
+          <Route path="/" element={<News setProgress = {this.setProgress} key="general" pageSize = {this.pageSize} country="us" category="General" />} />
+          <Route path="/Business" element={<News setProgress = {this.setProgress} key="business" pageSize = {this.pageSize} country="us" category="Business" />} />
+          <Route path="/Entertainment" element={<News setProgress = {this.setProgress} key="entertainment" pageSize = {this.pageSize} country="us" category="Entertainment" />} />
+          <Route path="/Sports" element={<News setProgress = {this.setProgress} key="sports" pageSize = {this.pageSize} country="us" category="Sports" />} />
+          <Route path="/Health" element={<News setProgress = {this.setProgress} key="health" pageSize = {this.pageSize} country="us" category="Health" />} />
+          <Route path="/Science" element={<News setProgress = {this.setProgress} key="science" pageSize = {this.pageSize} country="us" category="Science" />} />
+          <Route path="/Technology" element={<News setProgress = {this.setProgress} key="technology" pageSize = {this.pageSize} country="us" category="Technology" />} />
+          <Route path="/General" element={<News setProgress = {this.setProgress} key="general" pageSize = {this.pageSize} country="us" category="General" />} />
         </Routes>
       </Router>
     );
--- src/components/News.js
+++ src/components/News.js
@@ -32,14 +32,19 @@ export class News extends Component {
   }
 
   async updateNews() {
+    this.props.setProgress(10);
     const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=b587285e0408458abe7a87dec021e48d&page=${this.state.page}&pageSize=${this.props.pageSize}`;
     let data = await fetch(url);
+    this.props.setProgress(30);
     let parsedData = await data.json();
+    this.props.setProgress(50);
     this.setState({
       articles: parsedData.articles,
       totalResults: parsedData.totalResults,
     });
+    this.props.setProgress(100);
   }
+
   async componentDidMount() {
     this.updateNews();
   }
